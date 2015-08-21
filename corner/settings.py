d938678ffdde6c25e58853e9e1ff2179eed3a545
ieiayaobb <ieiayaobb@gmail.com>
2015-08-21 13:37:55 +0800

    modify MySQL connection configuration

--- corner/settings.py
+++ corner/settings.py
@@ -59,11 +59,12 @@ WSGI_APPLICATION = 'corner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'OPTIONS': {
-            'read_default_file': 'my.cnf',
-        },
+        'NAME': 'corner',
+        'USER': 'root',
+        'PASSWORD': '',
+        
     }
 }
 
